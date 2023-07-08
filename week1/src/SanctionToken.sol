33ce376163aa92c1b255f7f2139ea7daa06070ea
tommyrharper <thomasrobertharper@gmail.com>
2023-07-08 18:14:21 +0100

    feat: add ban function

--- week1/src/SanctionToken.sol
+++ week1/src/SanctionToken.sol
@@ -5,5 +5,11 @@ import "openzeppelin/token/ERC20/ERC20.sol";
 import "openzeppelin/access/Ownable.sol";
 
 contract SanctionToken is ERC20, Ownable {
+    mapping(address => bool) public blackList;
+
     constructor() ERC20("SanctionToken", "ST") {}
+
+    function ban(address _account, bool _banned) external onlyOwner {
+        blackList[_account] = _banned;
+    }
 }
