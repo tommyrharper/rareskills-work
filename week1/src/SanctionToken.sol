555c8da4e54e082c7d63f9456c174522d96ae95d
tommyrharper <thomasrobertharper@gmail.com>
2023-07-08 18:05:40 +0100

    feat: import ERC20

--- week1/src/SanctionToken.sol
+++ week1/src/SanctionToken.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract SanctionToken {
+import "openzeppelin/token/ERC20/ERC20.sol";
+
+contract SanctionToken is ERC20 {
     uint256 public number;
+
+    constructor() ERC20("SanctionToken", "ST") {}
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
