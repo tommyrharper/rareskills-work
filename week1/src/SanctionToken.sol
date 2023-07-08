e6cce5f91f15a54a7a3ee2b40ce952252b681d31
tommyrharper <thomasrobertharper@gmail.com>
2023-07-08 18:35:25 +0100

    feat: _beforeTokenTransfer checks

--- week1/src/SanctionToken.sol
+++ week1/src/SanctionToken.sol
@@ -7,9 +7,23 @@ import "openzeppelin/access/Ownable.sol";
 contract SanctionToken is ERC20, Ownable {
     mapping(address => bool) public blackList;
 
-    constructor() ERC20("SanctionToken", "ST") {}
+    constructor() ERC20("SanctionToken", "ST") {
+        _mint(msg.sender, 1000 ether);
+    }
 
     function ban(address _account, bool _banned) external onlyOwner {
         blackList[_account] = _banned;
     }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal view override {
+        if (blackList[to]) revert BannedToAddress(to);
+        if (blackList[from]) revert BannedFromAddress(from);
+    }
+
+    error BannedToAddress(address to);
+    error BannedFromAddress(address from);
 }
