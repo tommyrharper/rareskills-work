12ae9d6e426face7d11a4b0a9f3c764a1228ffd1
tommyrharper <thomasrobertharper@gmail.com>
2023-07-08 18:23:24 +0100

    test: ban is only owner

--- week1/test/SanctionToken.t.sol
+++ week1/test/SanctionToken.t.sol
@@ -23,9 +23,27 @@ contract SanctionTokenTest is Test {
         assertEq(sactionToken.symbol(), "ST");
     }
 
-    function testBan() public {
+    function testBanUpdatesMapping() public {
         sactionToken.ban(address(0x1), true);
         assertEq(sactionToken.blackList(address(0x1)), true);
+    }
+
+    function testUnBanUpdatesMapping() public {
+        sactionToken.ban(address(0x1), true);
+        assertEq(sactionToken.blackList(address(0x1)), true);
+
+        sactionToken.ban(address(0x1), false);
+        assertEq(sactionToken.blackList(address(0x1)), false);
+    }
+
+    function testBanIsOnlyOwner() public {
+        vm.prank(address(0x2));
+        vm.expectRevert("Ownable: caller is not the owner");
+        sactionToken.ban(address(0x1), true);
+    }
+
+    function testBanBlocksTransfers() public {
+
     }
 
 }
