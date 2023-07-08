669486d45306d6bc8342c77406c2dc05202b97a0
tommyrharper <thomasrobertharper@gmail.com>
2023-07-08 20:22:05 +0100

    test: update public to internal helper funcs

--- week1/test/TestHelpers.t.sol
+++ week1/test/TestHelpers.t.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.19;
 import {Test, console} from "forge-std/Test.sol";
 
 contract TestHelpers is Test {
-    uint256 public userNonce;
-    uint256 public nonce;
+    uint256 internal userNonce;
+    uint256 internal nonce;
 
-    function createAndDealUser() public returns (address) {
+    function createAndDealUser() internal returns (address) {
         address user = createUser();
         vm.deal(user, 100 ether);
         return user;
     }
 
     /// @dev create a new user address
-    function createUser() public returns (address) {
+    function createUser() internal returns (address) {
         userNonce++;
         return vm.addr(userNonce);
     }
