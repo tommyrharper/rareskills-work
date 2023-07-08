df9898b71435215c449c1ba14c63649af7fa920c
tommyrharper <thomasrobertharper@gmail.com>
2023-07-08 18:45:34 +0100

    test: transferFrom SanctionTOken

--- week1/test/SanctionToken.t.sol
+++ week1/test/SanctionToken.t.sol
@@ -5,54 +5,54 @@ import "./TestHelpers.t.sol";
 import "../src/SanctionToken.sol";
 
 contract SanctionTokenTest is TestHelpers {
-    SanctionToken public sactionToken;
+    SanctionToken public sanctionToken;
     address public user1;
     address public user2;
 
     function setUp() public {
-        sactionToken = new SanctionToken();
+        sanctionToken = new SanctionToken();
         user1 = createUser();
         user2 = createUser();
     }
 
     function testOwner() public {
-        assertEq(sactionToken.owner(), address(this));
+        assertEq(sanctionToken.owner(), address(this));
     }
 
     function testName() public {
-        assertEq(sactionToken.name(), "SanctionToken");
+        assertEq(sanctionToken.name(), "SanctionToken");
     }
 
     function testSymbol() public {
-        assertEq(sactionToken.symbol(), "ST");
+        assertEq(sanctionToken.symbol(), "ST");
     }
 
     function testBalance() public {
-        assertEq(sactionToken.balanceOf(address(this)), 1000 ether);
+        assertEq(sanctionToken.balanceOf(address(this)), 1000 ether);
     }
 
     function testBanUpdatesMapping() public {
-        sactionToken.ban(user1, true);
-        assertEq(sactionToken.blackList(user1), true);
+        sanctionToken.ban(user1, true);
+        assertEq(sanctionToken.blackList(user1), true);
     }
 
     function testUnBanUpdatesMapping() public {
-        sactionToken.ban(user1, true);
-        assertEq(sactionToken.blackList(user1), true);
+        sanctionToken.ban(user1, true);
+        assertEq(sanctionToken.blackList(user1), true);
 
-        sactionToken.ban(user1, false);
-        assertEq(sactionToken.blackList(user1), false);
+        sanctionToken.ban(user1, false);
+        assertEq(sanctionToken.blackList(user1), false);
     }
 
     function testBanIsOnlyOwner() public {
         vm.prank(address(user2));
         vm.expectRevert("Ownable: caller is not the owner");
-        sactionToken.ban(user1, true);
+        sanctionToken.ban(user1, true);
     }
 
     function testBanBlocksTransfersToBannedAddresses() public {
-        sactionToken.ban(user1, true);
-        assertEq(sactionToken.blackList(user1), true);
+        sanctionToken.ban(user1, true);
+        assertEq(sanctionToken.blackList(user1), true);
 
         vm.expectRevert(
             abi.encodeWithSelector(
@@ -60,12 +60,27 @@ contract SanctionTokenTest is TestHelpers {
                 user1
             )
         );
-        sactionToken.transfer(user1, 100);
+        sanctionToken.transfer(user1, 100);
+    }
+
+    function testBanBlocksTransfersFromToBannedAddresses() public {
+        sanctionToken.approve(address(this), 1000 ether);
+
+        sanctionToken.ban(user1, true);
+        assertEq(sanctionToken.blackList(user1), true);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                SanctionToken.BannedToAddress.selector,
+                user1
+            )
+        );
+        sanctionToken.transferFrom(address(this), user1, 100);
     }
 
     function testBanBlocksTransfersFromBannedAddresses() public {
-        sactionToken.ban(address(this), true);
-        assertEq(sactionToken.blackList(address(this)), true);
+        sanctionToken.ban(address(this), true);
+        assertEq(sanctionToken.blackList(address(this)), true);
 
         vm.expectRevert(
             abi.encodeWithSelector(
@@ -73,6 +88,21 @@ contract SanctionTokenTest is TestHelpers {
                 address(this)
             )
         );
-        sactionToken.transfer(user1, 100);
+        sanctionToken.transfer(user1, 100);
+    }
+
+    function testBanBlocksTransfersFromFromBannedAddresses() public {
+        sanctionToken.approve(address(this), 1000 ether);
+
+        sanctionToken.ban(address(this), true);
+        assertEq(sanctionToken.blackList(address(this)), true);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                SanctionToken.BannedFromAddress.selector,
+                address(this)
+            )
+        );
+        sanctionToken.transferFrom(address(this), user1, 100);
     }
 }
