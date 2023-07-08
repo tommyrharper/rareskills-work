6280a1c453e7348ec9e24ab59c0ca9c1b74e735a
tommyrharper <thomasrobertharper@gmail.com>
2023-07-08 19:08:11 +0100

    test: bonding token

---
+++ week1/test/BondingToken.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./TestHelpers.t.sol";
+import "../src/BondingToken.sol";
+
+contract BondingTokenTest is TestHelpers {
+    BondingToken public bondingToken;
+    address public user1;
+    address public user2;
+
+    function setUp() public {
+        bondingToken = new BondingToken();
+        user1 = createUser();
+        user2 = createUser();
+    }
+
+    function testName() public {
+        assertEq(bondingToken.name(), "BondingToken");
+    }
+
+    function testSymbol() public {
+        assertEq(bondingToken.symbol(), "BT");
+    }
+}
