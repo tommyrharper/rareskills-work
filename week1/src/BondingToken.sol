7764e0dbcbd3dcd8ab75c704a5308b17da71e131
tommyrharper <thomasrobertharper@gmail.com>
2023-07-08 19:30:33 +0100

    feat: buyBondingToken

--- week1/src/BondingToken.sol
+++ week1/src/BondingToken.sol
@@ -5,4 +5,8 @@ import "openzeppelin/token/ERC20/ERC20.sol";
 
 contract BondingToken is ERC20 {
     constructor() ERC20("BondingToken", "BT") {}
+
+    function buyBondingToken() external payable {
+        _mint(msg.sender, msg.value / 2);
+    }
 }
