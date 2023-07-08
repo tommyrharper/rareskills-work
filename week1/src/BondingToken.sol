c57af54fa9ce572fe32acc534439cdfdc9d0277d
tommyrharper <thomasrobertharper@gmail.com>
2023-07-08 19:58:32 +0100

    feat: buy bonding token math

--- week1/src/BondingToken.sol
+++ week1/src/BondingToken.sol
@@ -1,12 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {Test, console} from "forge-std/Test.sol";
+
 import "openzeppelin/token/ERC20/ERC20.sol";
+import "openzeppelin/utils/math/Math.sol";
 
 contract BondingToken is ERC20 {
+    uint256 public reserveBalance;
+
     constructor() ERC20("BondingToken", "BT") {}
 
     function buyBondingToken() external payable {
-        _mint(msg.sender, msg.value / 2);
+        if (msg.value == 0) revert MustPayGreaterThanZero();
+
+        uint256 oldReserveBalance = reserveBalance;
+        reserveBalance = oldReserveBalance + msg.value;
+
+        uint256 newSupply = Math.sqrt(2 * reserveBalance);
+        uint256 supplyChange = newSupply - totalSupply();
+
+
+        _mint(msg.sender, supplyChange);
     }
+
+    error MustPayGreaterThanZero();
 }
