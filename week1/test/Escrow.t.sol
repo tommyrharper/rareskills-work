76e995ef2497ba1742cc2a331335678b968d6b58
tommyrharper <thomasrobertharper@gmail.com>
2023-07-09 17:57:03 +0100

    test: create escrow

--- week1/test/Escrow.t.sol
+++ week1/test/Escrow.t.sol
@@ -3,17 +3,40 @@ pragma solidity 0.8.19;
 
 import "./TestHelpers.t.sol";
 import "../src/Escrow.sol";
+import "./MintableToken.t.sol";
 
 contract EscrowTest is TestHelpers {
     Escrow public escrow;
+    MintableToken public token;
     address public user1;
     address public user2;
 
     function setUp() public {
         escrow = new Escrow();
+        token = new MintableToken();
         user1 = createUser();
         user2 = createUser();
+
+        token.mint(address(this), 100 ether);
     }
 
-    function testSomething() public {}
+    function test_Create_Escrow_Entry() public {
+        token.approve(address(escrow), 100 ether);
+        escrow.createEscrow(address(token), user1, 100 ether);
+        (
+            address _token,
+            address _buyer,
+            address _seller,
+            uint256 _amount,
+            uint256 _withdrawalTime,
+            bool _withdrawn
+        ) = escrow.escrowEntries(0);
+
+        assertEq(_token, address(token));
+        assertEq(_buyer, address(this));
+        assertEq(_seller, user1);
+        assertEq(_amount, 100 ether);
+        assertEq(_withdrawalTime, block.timestamp + 3 days);
+        assertEq(_withdrawn, false);
+    }
 }
