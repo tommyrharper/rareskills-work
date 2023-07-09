517301f081992162aac4b748bf2b8511010ec5c4
tommyrharper <thomasrobertharper@gmail.com>
2023-07-09 18:07:23 +0100

    test: withdraw funds

--- week1/test/Escrow.t.sol
+++ week1/test/Escrow.t.sol
@@ -6,10 +6,18 @@ import "../src/Escrow.sol";
 import "./MintableToken.t.sol";
 
 contract EscrowTest is TestHelpers {
+    /*//////////////////////////////////////////////////////////////
+                                 STATE
+    //////////////////////////////////////////////////////////////*/
+
     Escrow public escrow;
     MintableToken public token;
     address public user1;
     address public user2;
+
+    /*//////////////////////////////////////////////////////////////
+                                 SETUP
+    //////////////////////////////////////////////////////////////*/
 
     function setUp() public {
         escrow = new Escrow();
@@ -19,6 +27,10 @@ contract EscrowTest is TestHelpers {
 
         token.mint(address(this), 100 ether);
     }
+
+    /*//////////////////////////////////////////////////////////////
+                          CREATE ESCROW TESTS
+    //////////////////////////////////////////////////////////////*/
 
     function test_Create_Escrow_Entry() public {
         assertEq(token.balanceOf(address(escrow)), 0);
@@ -54,5 +66,22 @@ contract EscrowTest is TestHelpers {
     function test_Cannot_Create_Entry_With_EOA_Token_Address() public {
         vm.expectRevert("Address: call to non-contract");
         escrow.createEscrow(user2, user1, 100 ether);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            WITHDRAWAL TESTS
+    //////////////////////////////////////////////////////////////*/
+
+    function test_Withdraw_Funds() public {
+        token.approve(address(escrow), 100 ether);
+        escrow.createEscrow(address(token), user1, 100 ether);
+
+        vm.warp(block.timestamp + 3 days);
+        
+        vm.prank(user1);
+        escrow.withdraw(0, user1);
+
+        assertEq(token.balanceOf(user1), 100 ether);
+        assertEq(token.balanceOf(address(escrow)), 0);
     }
 }
