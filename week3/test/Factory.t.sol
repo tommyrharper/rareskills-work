5927f9020c8f68ddc412b8d92e51cc1521bebe48
tommyrharper <thomasrobertharper@gmail.com>
2023-07-22 18:52:15 +0100

    feat: factory test

--- week3/test/Factory.t.sol
+++ week3/test/Factory.t.sol
@@ -11,5 +11,13 @@ contract FactoryTest is Test {
         factory = new Factory(address(this));
     }
 
-    function testFactory() public {}
+    function testFactory() public {
+        address tokenA = address(0x1);
+        address tokenB = address(0x2);
+        address pair = factory.createPair(tokenA, tokenB);
+        assertEq(factory.allPairsLength(), 1);
+        assertEq(factory.allPairs(0), pair);
+        assertEq(factory.getPair(tokenA, tokenB), pair);
+        assertEq(factory.getPair(tokenB, tokenA), pair);
+    }
 }
