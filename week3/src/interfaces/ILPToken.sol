32528853083af03609a09c0dcbff9be0bac9fa9e
tommyrharper <thomasrobertharper@gmail.com>
2023-07-22 13:25:44 +0100

    delete: old comments

--- week3/src/interfaces/ILPToken.sol
+++ week3/src/interfaces/ILPToken.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.5.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
 interface ILPToken {
     event Approval(address indexed owner, address indexed spender, uint value);
