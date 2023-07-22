04b92859a7037e0930d125d41b3e5decd733157e
tommyrharper <thomasrobertharper@gmail.com>
2023-07-22 18:56:32 +0100

    setup tests

--- week3/test/SwapPair.t.sol
+++ week3/test/SwapPair.t.sol
@@ -3,13 +3,25 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/SwapPair.sol";
+import "../src/Factory.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract SwapPairTest is Test {
+    Factory public factory;
     SwapPair public swapPair;
+    IERC20 public tokenA;
+    IERC20 public tokenB;
 
     function setUp() public {
-        swapPair = new SwapPair();
+        tokenA = new ERC20("TokenA", "TA");
+        tokenB = new ERC20("TokenB", "TB");
+        factory = new Factory(address(this));
+        address pair = factory.createPair(address(tokenA), address(tokenB));
+        swapPair = SwapPair(pair);
     }
 
-    function testSwapPair() public {}
+    function test_Mint() public {
+        // swapPair.mint(address(this));
+    }
 }
