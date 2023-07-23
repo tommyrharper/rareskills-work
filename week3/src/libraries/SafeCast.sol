738692042575bc4f223bef52bd4ddc6337d35bfd
tommyrharper <thomasrobertharper@gmail.com>
2023-07-23 19:45:10 +0100

    add decimal math synthetix

---
+++ week3/src/libraries/SafeCast.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.11 <0.9.0;
+
+/**
+ * Utilities that convert numeric types avoiding silent overflows.
+ */
+import "./SafeCast/SafeCastI256.sol";
+import "./SafeCast/SafeCastU256.sol";
