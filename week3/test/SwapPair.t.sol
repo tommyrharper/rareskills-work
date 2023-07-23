16bf567b8c1794fe1d2b97efdb48ee90bfbf4ca3
tommyrharper <thomasrobertharper@gmail.com>
2023-07-23 11:50:18 +0100

    refactor: use helpers in mint tests

--- week3/test/SwapPair.t.sol
+++ week3/test/SwapPair.t.sol
@@ -34,16 +34,13 @@ contract SwapPairTest is Test {
     //////////////////////////////////////////////////////////////*/
 
     function test_Equal_First_Mint() public {
-        tokenA.mint(address(swapPair), 10_000);
-        tokenB.mint(address(swapPair), 10_000);
-        swapPair.mint(address(this));
+        sendAndMint(10_000);
         assertEq(swapPair.balanceOf(address(this)), 9_000);
     }
 
     function test_Equal_First_Mint_Fuzz(uint64 amount) public {
         uint256 minLiquidity = swapPair.MINIMUM_LIQUIDITY();
-        tokenA.mint(address(swapPair), amount);
-        tokenB.mint(address(swapPair), amount);
+        sendTokensToSwapPair(amount);
         if (amount <= minLiquidity) {
             vm.expectRevert();
             swapPair.mint(address(this));
@@ -54,9 +51,7 @@ contract SwapPairTest is Test {
     }
 
     function test_Unequal_First_Mint() public {
-        tokenA.mint(address(swapPair), 25_000);
-        tokenB.mint(address(swapPair), 1_000);
-        swapPair.mint(address(this));
+        sendAndMint(25_000, 1_000);
         assertEq(swapPair.balanceOf(address(this)), 4_000);
     }
 
@@ -68,8 +63,7 @@ contract SwapPairTest is Test {
         uint256 amountB = _amountB;
         uint256 minLiquidity = swapPair.MINIMUM_LIQUIDITY();
         uint256 sqrt = Math.sqrt(amountA * amountB);
-        tokenA.mint(address(swapPair), amountA);
-        tokenB.mint(address(swapPair), amountB);
+        sendTokensToSwapPair(amountA, amountB);
         if (sqrt <= minLiquidity) {
             vm.expectRevert();
             swapPair.mint(address(this));
@@ -84,24 +78,17 @@ contract SwapPairTest is Test {
     //////////////////////////////////////////////////////////////*/
 
     function test_Equal_Second_Mint() public {
-        tokenA.mint(address(swapPair), 10_000);
-        tokenB.mint(address(swapPair), 10_000);
-        swapPair.mint(address(this));
+        sendAndMint(10_000);
         assertEq(swapPair.balanceOf(address(this)), 9_000);
-        tokenA.mint(address(swapPair), 10_000);
-        tokenB.mint(address(swapPair), 10_000);
-        swapPair.mint(address(this));
+        sendAndMint(10_000);
         assertEq(swapPair.balanceOf(address(this)), 19_000);
     }
 
     function test_Equal_Second_Mint_Fuzz(uint64 _amount) public {
         uint256 amount = _amount;
-        tokenA.mint(address(swapPair), 10_000);
-        tokenB.mint(address(swapPair), 10_000);
-        swapPair.mint(address(this));
+        sendAndMint(10_000);
         assertEq(swapPair.balanceOf(address(this)), 9_000);
-        tokenA.mint(address(swapPair), amount);
-        tokenB.mint(address(swapPair), amount);
+        sendTokensToSwapPair(amount);
         if (amount <= 0) {
             vm.expectRevert("UniswapV2: INSUFFICIENT_LIQUIDITY_MINTED");
             swapPair.mint(address(this));
@@ -115,13 +102,26 @@ contract SwapPairTest is Test {
                                 HELPERS
     //////////////////////////////////////////////////////////////*/
 
-    function initialMint(uint256 amount) public {
+    function sendAndMint(uint256 amount) public {
         sendTokensToSwapPair(amount);
+        swapPair.mint(address(this));
+    }
+
+    function sendAndMint(uint256 tokenAAmount, uint256 tokenBAmount) public {
+        sendTokensToSwapPair(tokenAAmount, tokenBAmount);
         swapPair.mint(address(this));
     }
 
     function sendTokensToSwapPair(uint256 amount) public {
         tokenA.mint(address(swapPair), amount);
         tokenB.mint(address(swapPair), amount);
+    }
+
+    function sendTokensToSwapPair(
+        uint256 tokenAAmount,
+        uint256 tokenBAmount
+    ) public {
+        tokenA.mint(address(swapPair), tokenAAmount);
+        tokenB.mint(address(swapPair), tokenBAmount);
     }
 }
