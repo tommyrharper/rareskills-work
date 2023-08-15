2781d9411bec2ed52c1cd077f2f860b40e1d7006
tommyrharper <thomasrobertharper@gmail.com>
2023-08-15 15:02:58 +0100

    delete: MintableERC20

--- week6/dex/test/Dex.t.sol
+++ week6/dex/test/Dex.t.sol
@@ -4,14 +4,6 @@ pragma solidity ^0.8.9;
 import "../contracts/Dex.sol";
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
-contract MintableERC20 is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
-
-    function mint(address account, uint256 amount) public {
-        _mint(account, amount);
-    }
-}
-
 /// @dev Run the template with
 ///      ```
 ///      solc-select use 0.8.20
