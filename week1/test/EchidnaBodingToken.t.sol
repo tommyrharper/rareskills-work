7309fa7ac4859b7afa687bab397a614b1dba2551
tommyrharper <thomasrobertharper@gmail.com>
2023-08-15 17:15:45 +0100

    feat: add bonding token echidna tests

---
+++ week1/test/EchidnaBodingToken.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "../src/BondingToken.sol";
+
+// import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+/// @dev Run the template with
+///      ```
+///      solc-select use 0.8.20
+///      echidna ./ --contract EchidnaBodingToken
+///      echidna ./week1 --contract EchidnaBodingToken
+///      ```
+contract EchidnaBodingToken is BondingToken {
+    function echidna_test_sufficient_eth_for_reserve()
+        public
+        view
+        returns (bool)
+    {
+        return reserveBalance >= address(this).balance;
+    }
+
+    function test_correct_total_supply() public view returns (bool) {
+        return totalSupply == reserveBalance;
+    }
+}
