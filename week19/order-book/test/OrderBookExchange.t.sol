acb74712d3d63bf1486338cb81d6b2225e4caf4f
tommyrharper <thomasrobertharper@gmail.com>
2024-01-13 21:21:32 +0000

    refactor: add test helpers contract

--- week19/order-book/test/OrderBookExchange.t.sol
+++ week19/order-book/test/OrderBookExchange.t.sol
@@ -5,52 +5,15 @@ import {Test, console} from "forge-std/Test.sol";
 import {OrderBookExchange} from "../src/OrderBookExchange.sol";
 import {PermitToken} from "../src/PermitToken.sol";
 import {SigUtils, Permit} from "./SigUtils.sol";
+import {OrderBookExchangeTestHelpers} from "./OrderBookExchangeTestHelpers.sol";
 
-contract OrderBookExchangeTest is Test {
-    PermitToken internal tokenA;
-    PermitToken internal tokenB;
-    OrderBookExchange internal orderBookExchange;
-
-    SigUtils internal sigUtils;
-
-    uint256 internal user1PrivateKey;
-    uint256 internal user2PrivateKey;
-    address internal user1;
-    address internal user2;
-
-    struct PermitSig {
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
-    }
-
-    function setUp() public {
-        user1PrivateKey = 0xA11CE;
-        user1 = vm.addr(user1PrivateKey);
-        user2PrivateKey = 0xFACADE;
-        user2 = vm.addr(user2PrivateKey);
-
-        tokenA = new PermitToken("TokenA", "A", user1);
-        tokenB = new PermitToken("TokenB", "B", user2);
-        orderBookExchange = new OrderBookExchange();
-
-        sigUtils = new SigUtils();
-    }
-
+contract OrderBookExchangeTest is OrderBookExchangeTestHelpers {
     function test_permit_tokenA() public {
-        (Permit memory permit, uint8 v, bytes32 r, bytes32 s) = sigUtils
-            .getSignedPermit(tokenA, user1PrivateKey, user2, 1 ether);
-
-        vm.prank(user2);
-        tokenA.permit(
-            permit.owner,
-            permit.spender,
-            permit.value,
-            permit.deadline,
-            v,
-            r,
-            s
+        (Permit memory permit, uint8 v, bytes32 r, bytes32 s) = getTokenAPermit(
+            1 ether
         );
+
+        executePermit(permit, v, r, s);
 
         assertEq(tokenA.balanceOf(user2), 0);
 
@@ -61,19 +24,11 @@ contract OrderBookExchangeTest is Test {
     }
 
     function test_permit_tokenB() public {
-        (Permit memory permit, uint8 v, bytes32 r, bytes32 s) = sigUtils
-            .getSignedPermit(tokenB, user2PrivateKey, user1, 1 ether);
-
-        vm.prank(user1);
-        tokenB.permit(
-            permit.owner,
-            permit.spender,
-            permit.value,
-            permit.deadline,
-            v,
-            r,
-            s
+        (Permit memory permit, uint8 v, bytes32 r, bytes32 s) = getTokenBPermit(
+            1 ether
         );
+
+        executePermit(permit, v, r, s);
 
         assertEq(tokenB.balanceOf(user1), 0);
 
@@ -82,5 +37,4 @@ contract OrderBookExchangeTest is Test {
 
         assertEq(tokenB.balanceOf(user1), 1 ether);
     }
-
 }
---
+++ week19/order-book/test/OrderBookExchangeTestHelpers.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {OrderBookExchange} from "../src/OrderBookExchange.sol";
+import {PermitToken} from "../src/PermitToken.sol";
+import {SigUtils, Permit} from "./SigUtils.sol";
+
+contract OrderBookExchangeTestHelpers is Test {
+    PermitToken internal tokenA;
+    PermitToken internal tokenB;
+    OrderBookExchange internal orderBookExchange;
+
+    SigUtils internal sigUtils;
+
+    uint256 internal user1PrivateKey;
+    uint256 internal user2PrivateKey;
+    address internal user1;
+    address internal user2;
+
+    struct PermitSig {
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+    }
+
+    function setUp() public {
+        user1PrivateKey = 0xA11CE;
+        user1 = vm.addr(user1PrivateKey);
+        user2PrivateKey = 0xFACADE;
+        user2 = vm.addr(user2PrivateKey);
+
+        tokenA = new PermitToken("TokenA", "A", user1);
+        tokenB = new PermitToken("TokenB", "B", user2);
+        orderBookExchange = new OrderBookExchange();
+
+        sigUtils = new SigUtils();
+    }
+
+    function executePermit(
+        Permit memory permit,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) internal {
+        if (permit.owner == user1) {
+            tokenA.permit(
+                permit.owner,
+                permit.spender,
+                permit.value,
+                permit.deadline,
+                v,
+                r,
+                s
+            );
+        } else {
+            tokenB.permit(
+                permit.owner,
+                permit.spender,
+                permit.value,
+                permit.deadline,
+                v,
+                r,
+                s
+            );
+        }
+    }
+
+    function getTokenAPermit(
+        uint256 _value
+    )
+        internal
+        view
+        returns (Permit memory permit, uint8 v, bytes32 r, bytes32 s)
+    {
+        return sigUtils.getSignedPermit(tokenA, user1PrivateKey, user2, _value);
+    }
+
+    function getTokenBPermit(
+        uint256 _value
+    )
+        internal
+        view
+        returns (Permit memory permit, uint8 v, bytes32 r, bytes32 s)
+    {
+        return sigUtils.getSignedPermit(tokenB, user2PrivateKey, user1, _value);
+    }
+}
