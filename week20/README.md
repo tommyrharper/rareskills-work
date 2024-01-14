3d39ec954587b95a9984a38ef2214e9c784d8680
tommyrharper <thomasrobertharper@gmail.com>
2024-01-14 18:07:54 +0000

    feat: vanity address generation complete

--- week20/README.md
+++ week20/README.md
@@ -4,7 +4,14 @@
 
 - [x]  Given a random point *n*, is *n* a valid point on the sepc265k1 curve? Use any language you like. Your function should take *****n***** and return true or false. **Do not use a library to implement this**.
   - [My solution](./sepc265k1.py)
-- [ ]  Use the code in the blog post above to generate an address that starts with 0x0000… (there 2 bytes of leading zeros). Be very careful where you get your randomness from!
+- [x]  Use the code in the blog post above to generate an address that starts with 0x0000… (there 2 bytes of leading zeros). Be very careful where you get your randomness from!
+  - [My solution](./generate-keys/generate-keys.py)
+```bash
+# Private Key: (don't steal plz only like ~15 ETH on here and a few k OP)
+0xbd57d237176728d4fa5cb5bab8b90a57b1bc33d3787c059437845cd98fdf2b0a
+# Eth Address:
+0x000094d2b4a0c2cf043bb2951237264c13c3cf52
+```
 - [ ]  Capture the ether [Fuzzy Identitiy](https://capturetheether.com/challenges/accounts/fuzzy-identity/)
 
 
--- week20/generate-keys/generate-keys.py
+++ week20/generate-keys/generate-keys.py
@@ -3,18 +3,29 @@ from ecpy.curves import Curve
 from ecpy.keys import ECPublicKey, ECPrivateKey
 from sha3 import keccak_256
 
-private_key = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-
+# used an online random number generator to generate a 256-bit number, then went and randomly flipped ~25 bits manually for extra security
+result = b'1011110101010111110100100011011100010111011001110010100011010100111110100101110010110101101110101011100010111001000010100101011110110001101111000011001111010011011110000111110000000101100101000011011110000100010111001101100110001111110110111011010110110011'
+private_key = int(result, 2)
 cv = Curve.get_curve('secp256k1')
-pv_key = ECPrivateKey(private_key, cv)
-pu_key = pv_key.get_public_key()
 
-# equivalent alternative for illustration:
-# concat_x_y = bytes.fromhex(hex(pu_key.W.x)[2:] + hex(pu_key.W.y)[2:])
+def get_eth_addr(pk):
+    pv_key = ECPrivateKey(pk, cv)
+    pu_key = pv_key.get_public_key()
 
-concat_x_y = pu_key.W.x.to_bytes(32, byteorder='big') + pu_key.W.y.to_bytes(32, byteorder='big')
-eth_addr = '0x' + keccak_256(concat_x_y).digest()[-20:].hex()
+    # equivalent alternative for illustration:
+    # concat_x_y = bytes.fromhex(hex(pu_key.W.x)[2:] + hex(pu_key.W.y)[2:])
+
+    concat_x_y = pu_key.W.x.to_bytes(32, byteorder='big') + pu_key.W.y.to_bytes(32, byteorder='big')
+    eth_addr = '0x' + keccak_256(concat_x_y).digest()[-20:].hex()
+    return eth_addr
+
+eth_addr = get_eth_addr(private_key)
+
+for i in range(2_000_000):
+    eth_addr = get_eth_addr(private_key)
+    if eth_addr[:6] == '0x0000':
+        break
+    private_key += 1
 
 print('private key: ', hex(private_key))
 print('eth_address: ', eth_addr)
-
