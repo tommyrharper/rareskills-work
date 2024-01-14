f790df7c821b3ef5d7894f7479c9b9566ed2b30e
tommyrharper <thomasrobertharper@gmail.com>
2024-01-14 20:49:08 +0000

    feat: setup week22 exercise3

---
+++ week21/exercise-3/test/Week22Exercise3.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Week22Exercise3} from "../src/Week22Exercise3.sol";
+
+contract Week22Exercise3Test is Test {
+    Week22Exercise3 internal exercise;
+
+    uint256 privateKey =
+        0x1010101010101010101010101010101010101010101010101010101010101010;
+    address internal owner;
+
+    function setUp() public {
+        owner = vm.addr(privateKey);
+        exercise = new Week22Exercise3();
+    }
+
+    function test_attack() public {
+        exercise.claimAirdrop(100, address(this), 0, bytes32(0), bytes32(0));
+    }
+}
