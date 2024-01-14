32ca442f2b54bf9ec253d2847aff584e834be3b6
tommyrharper <thomasrobertharper@gmail.com>
2024-01-14 23:58:06 +0000

    💥 week22 exercise 2 complete

--- week21/exercise-2/test/Week22Exercise2.t.sol
+++ week21/exercise-2/test/Week22Exercise2.t.sol
@@ -37,24 +37,52 @@ contract Week22Exercise2Test is Test {
     //     exercise.challenge(message, signature);
     // }
 
-    function test_attack() public {
-        bool success = false;
-        uint256 num;
-        while (!success) {
-            string memory message = "attack";
-            bytes32 msgHash = keccak256(abi.encode(num))
-                .toEthSignedMessageHash();
+    // 0xe5d0b13209c030a26b72ddb84866ae7b32f806d64f28136cb5516ab6ca15d3c438d9e7c79efa063198fda1a5b48e878a954d79372ed71922003f847029bf2e751b
+    // e5d0b13209c030a26b72ddb84866ae7b32f806d64f28136cb5516ab6ca15d3c4
+    // 38d9e7c79efa063198fda1a5b48e878a954d79372ed71922003f847029bf2e75
+    // 1b
 
-            (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, msgHash);
-
-            bytes memory signature = abi.encodePacked(r, s, v);
-
-            try exercise.challenge(message, signature) {
-                console.log("num:", num);
-                success = true;
-            } catch {
-                num++;
-            }
-        }
+    function getSignature() internal returns (bytes memory) {
+        bytes32 r = 0xe5d0b13209c030a26b72ddb84866ae7b32f806d64f28136cb5516ab6ca15d3c4;
+        bytes32 s = 0x38d9e7c79efa063198fda1a5b48e878a954d79372ed71922003f847029bf2e75;
+        uint8 v = 27;
+        return abi.encodePacked(r, s, v);
     }
+
+    function test_attack() public {
+        string memory message = "attack at dawn";
+        bytes memory signature = getSignature();
+
+        // bytes32 msgHash = keccak256(abi.encode(message))
+        //     .toEthSignedMessageHash();
+
+        // (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, msgHash);
+
+        // bytes memory signature = abi.encodePacked(r, s, v);
+        // assertEq(signature.length, 65);
+
+        exercise.challenge(message, signature);
+    }
+
+
+    // function test_attack() public {
+    //     bool success = false;
+    //     uint256 num;
+    //     while (!success) {
+    //         string memory message = "attack at dawn";
+    //         bytes32 msgHash = keccak256(abi.encode(num))
+    //             .toEthSignedMessageHash();
+
+    //         (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, msgHash);
+
+    //         bytes memory signature = abi.encodePacked(r, s, v);
+
+    //         try exercise.challenge(message, signature) {
+    //             console.log("num:", num);
+    //             success = true;
+    //         } catch {
+    //             num++;
+    //         }
+    //     }
+    // }
 }
